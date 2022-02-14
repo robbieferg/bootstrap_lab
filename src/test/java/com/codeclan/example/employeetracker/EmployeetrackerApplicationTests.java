b0736863517f91366182c95eaba5854e9f6ca111
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-14 14:46:19 +0000

    MVP complete

--- src/test/java/com/codeclan/example/employeetracker/EmployeetrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/employeetracker/EmployeetrackerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.employeetracker;
 
+import com.codeclan.example.employeetracker.models.Employee;
+import com.codeclan.example.employeetracker.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeetrackerApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canSaveEmployee() {
+		Employee bill = new Employee("Bill", 35, 89076, "bill@bill.com");
+		employeeRepository.save(bill);
 	}
 
 }
