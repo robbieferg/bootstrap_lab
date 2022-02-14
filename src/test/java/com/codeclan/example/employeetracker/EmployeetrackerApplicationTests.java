5769b3e61d5606c368afdc8d737fc50911be0983
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-14 14:10:01 +0000

    Initial commit

---
+++ src/test/java/com/codeclan/example/employeetracker/EmployeetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
